90741c45bf96038daff07a0ec18a9ca3fd3dcf56
akki1821 <pppatelakash@gmail.com>
2022-03-29 06:07:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const moves=document.getElementById("move");
+const images=document.querySelectorAll("img");
+let start=0;
+
+function move (){
+    start++;
+    if (start>images.length-1)
+    {start=0;}
+    moves.style.transform=`translateX(${-start*80}%)`;
+}
+setInterval(move,700);
